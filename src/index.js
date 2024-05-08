a766f64f8b1e2fe2eba4d80f35b00fa6289ad0e4
shivasinde <shankarvshinde59@gmail.com>
2024-05-08 12:47:00 +0530

    connect mongo atls

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({path:"./env"})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+
+(async()=>{
+  try {
+    const connectDB = await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`)
+    app.on("error",(err)=>{
+      console.log("not connect with database" , err);
+      throw err;
+    })
+    app.listen(process.env.PORT,()=>{
+      console.log(`listening on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.log("Error",error);
+    throw error;
+  }
+})()
+
+
+*/
